bc08013cd68281419e205adc84f6f0b14902986c
tw1911 <tw1911@yandex.ru>
2019-08-30 18:44:11 +0300

    Initial commit

---
+++ src/main/java/ru/tw1911/test/universalStub/UniversalStubApplication.java
@@ -0,0 +1,13 @@
+package ru.tw1911.test.universalStub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalStubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversalStubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/tw1911/test/universalStub/UniversalStubApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.tw1911.test.universalStub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UniversalStubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
